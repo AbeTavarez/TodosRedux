254660c0e7ae9622eaded346e1b27e14bfa46200
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-02-21 14:45:12 -0500

    path fixed

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import rootReducer from './reducer/index';
+import rootReducer from './reducers/index';
 
 const initialState = {};
 
